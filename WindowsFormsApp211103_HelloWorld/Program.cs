b0305555ae172ff6fa2a3b8066019b3ea34787b8
hideru <jota.16.stigma.world@gmail.com>
2021-11-03 17:51:18 +0900

    Hello World Succeeded

---
+++ WindowsFormsApp211103_HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp211103_HelloWorld
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Form1 form1 = new Form1();
+            // Form1 form = new Form1();
+            // form1.Text = "Hello World!";
+            // form.Text = "Hello World!";
+            // Form form = new Form();
+            // form.Text = "Hello World!";
+            
+            // ^ Do not write the code that defines New Form here. It should write under these two Application
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            Form1 form1 = new Form1();
+            form1.Text = "Hello World!";
+            
+            // Application.Run(new Form1()); // Succeeded
+            Application.Run(form1);
+        }
+    }
+}
